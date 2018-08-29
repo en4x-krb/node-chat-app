10502c7f398ad04ac82da111b8fda128bf918791
Enax KRB <en4x@krb.com>
2018-08-30 00:35:44 +0530

    socketio

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 var app = express();
+var server = http.createServer(app);
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-// console.log(publicPath);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
+server.listen(port, () => {console.log(`Listening on port ${port}`)});
