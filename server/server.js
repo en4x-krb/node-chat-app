681998f689f8be5299027088daab04ac3899e5f7
Enax KRB <en4x@krb.com>
2018-08-30 00:04:43 +0530

    package-json

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 var app = express();
 
 const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
 // console.log(publicPath);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {console.log('Listening on port 3000')});
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
